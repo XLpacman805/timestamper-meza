4d2399f1eb7dcabb77bd970b8e699744cbf7673e
System <none>
2017-08-11 00:34:35 +0000

    Cleaned some code by making the ClockWork function

--- server.js
+++ server.js
@@ -6,33 +6,26 @@ const express = require('express');
 const app = express();
 const strf = require("strftime");
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('views'));
+
+function clockWork(d){//d is a date object
+  return JSON.stringify({
+    unix: d.getTime()/1000,
+    natural: (d.toDateString() === "Invalid Date") ? null : strf("%B %d %Y",d)
+  });
+}
 
 app.get("/api/:t", (req, res)=>{
   var time = req.params.t;
   //check for unix time
   if(isNaN(parseInt(time))){//time is a string
     var d = new Date(time);
-    res.end(JSON.stringify(
-      {
-      unix: d.getTime()/1000,
-      natural: (d.toUTCString() === "Invalid Date") ? null : d.toDateString() //is utc string an invalid date? If so return null, if not then return the utcdate. 
-    }));
-    
+    res.end(clockWork(d));
   }else if(!isNaN(parseInt(time))){ //time is number
-    var time_unix = parseInt(time);
-    var dx = new Date(time_unix*1000);
-    console.log(time_unix);
-    console.log(dx);
-    res.end(JSON.stringify(
-      {
-      unix: dx.getTime()/1000,
-      natural: (dx.toUTCString() === "Invalid Date") ? null : dx.toDateString()
-      }));
+    time = parseInt(time);
+    var d = new Date(time*1000);//Date requires milliseconds
+    res.end(clockWork(d));
   }  
   
 });
